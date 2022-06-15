1bbae75aa6a9403342ca81b6acb04d39d5535444
Private <msaleh.abdelrahman@gmail.com>
2022-06-16 00:08:14 +0200

    added back and apply buttons to job details

--- src/components/JobDetails/JobDetails.js
+++ src/components/JobDetails/JobDetails.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
+import {NavLink, useParams} from "react-router-dom";
 import {Image} from "primereact/image";
+import {Button} from "primereact/button";
 
 let JobDetails = () => {
 
@@ -27,24 +28,44 @@ let JobDetails = () => {
         </div>)
     }
 
+    const backButton = (
+            <NavLink to={'/jobs'} className={'btn'}>
+                <Button className={'p-button p-button-outlined p-button-rounded'}
+                        label={'Back'}></Button>
+            </NavLink>
+    )
+
+    const applyButton = (
+            <NavLink to={'...'} className={'btn'}>
+                <Button className={'p-button p-button-outlined p-button-rounded'}
+                        label={'Apply'}></Button>
+            </NavLink>
+    )
+
     let renderJob = () => {
         if (job.id) {
-            return (<div>
-                <div className={'d-flex align-items-center justify-content-center text-center row'}>
-                    <div className={'d-flex-column col-sm-12 col-lg-6 col-md-6 order-1 order-lg-0 order-md-0'}>
-                        <p className={'h3'}>
-                            {job.name}
-                        </p>
-                        <p>
-                            {job.Description}
-                        </p>
+            return (
+                <div className={'container my-5 py-5 vh-10 row here'}>
+                    <div className={'d-flex align-items-center justify-content-center text-center row col-lg-11 col-sm-12'}>
+                        <div className={'d-flex-column col-sm-12 col-lg-6 col-md-6 order-1 order-lg-0 order-md-0'}>
+                            <p className={'h3'}>
+                                {job.name}
+                            </p>
+                            <p>
+                                {job.Description}
+                            </p>
+                        </div>
+                        <div className={'col-sm-12 col-lg-6 col-md-6 order-0 order-lg-1 order-md-1'}>
+                            <Image preview className={'rounded img-fluid'} name={`${job.name} image`} width={'200'}
+                                   alt={`${job.name}`} src={job.image}></Image>
+                        </div>
                     </div>
-                    <div className={'col-sm-12 col-lg-6 col-md-6 order-0 order-lg-1 order-md-1'}>
-                        <Image preview className={'rounded img-fluid'} name={`${job.name} image`} width={'200'}
-                               alt={`${job.name}`} src={job.image}></Image>
+                    <div className={'d-flex flex-column col-lg-1'}>
+                        {applyButton}
+                        {backButton}
                     </div>
                 </div>
-            </div>)
+            )
         } else {
             error('no data')
         }
