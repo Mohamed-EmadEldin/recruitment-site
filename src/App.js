4af5988d64d34ccb9bc75bca70255fcaa7548eb3
Private <msaleh.abdelrahman@gmail.com>
2022-06-15 15:48:04 +0200

    duplicated import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import logo from './logo.svg';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 import './App.css';
-import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
-import "primereact/resources/primereact.min.css";                  //core css
-import "primeicons/primeicons.css";
 
 import Signup from "./components/signup";
 import Login from "./components/login";
