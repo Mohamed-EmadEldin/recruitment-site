5acf5224a0206039b46beafb21cda11cb864f46b
Private <msaleh.abdelrahman@gmail.com>
2022-06-17 00:47:06 +0200

    styling and tags preparation

--- src/components/AddJop/AddJop.js
+++ src/components/AddJop/AddJop.js
@@ -1,7 +1,10 @@
 import {useEffect, useState} from "react";
 import {InputText} from 'primereact/inputtext';
 import {InputTextarea} from 'primereact/inputtextarea';
+import {Button} from "primereact/button";
+import {MultiSelect} from "primereact/multiselect";
 
+import './AddJop.css'
 
 let CreateForm = () => {
 
@@ -9,6 +12,8 @@ let CreateForm = () => {
     let [name, setName] = useState('')
     let [description, setDescription] = useState('')
     let [image, setImage] = useState('')
+    let [tags, setTags] = useState([])
+    let [selectedTags, setSelectedTags] = useState(null);
 
 
     let create = async () => {
@@ -26,6 +31,7 @@ let CreateForm = () => {
                 "image": image,
                 "created_by": 3, //TODO: remove after the new serializer
                 "status": "O"
+                //TODO: add tags
             })
         });
         return response.json();
@@ -52,40 +58,67 @@ let CreateForm = () => {
         }
     }
 
+    let getTags = () => {
+        return [
+            {
+                name: '1', code: 'one'
+            },
+            {
+                name: '2', code: 'two'
+            }
+        ]
+    }
+
     useEffect(() => {
         setToken(localStorage.getItem('token') || '')
+        setTags(getTags())
     }, [])
+
 
     let loginForm = () => {
         return (
             <div className={'d-flex justify-content-center align-content-center vh-100'}>
-                <form action="#">
-                        <span className="p-float-label">
-                        <InputText type={'text'} id={'name'} className={'my-2'}
+                <form action="#"
+                      className={'d-flex flex-column container border border-secondary border-opacity-50'}>
+                    <p className={'h4 text-center'}>Create Job</p>
+                    <div className="field">
+                        <label htmlFor="job-name" className="text-start block">Job name</label>
+                        <InputText type={'text'} id={'job-name'} className={'my-2 form-control'}
                                    onChange={(Event) => {
                                        setName(Event.target.value)
                                    }}
                         />
-                                <label htmlFor="name">Job name</label>
-                        </span>
-                    <span className={'p-float-label'}>
-                    <InputTextarea rows={5} cols={30} className={'my-2'} maxLength={250} autoResize
-                                   onChange={(Event) => {
-                                       setDescription(Event.target.value)
-                                   }}
-                    />
-                            <label htmlFor="description">Description</label>
-                    </span>
-                    <span className="p-float-label">
-                        <InputText type={'text'} id={'image'} className={'my-2'}
+                        <small id="jop-name-help" className="text-start block">Enter the job's name.</small>
+                    </div>
+                    <div className="field">
+                        <label htmlFor="description" className="text-start block">Description</label>
+                        <InputTextarea id={"description"} rows={5} cols={30} className={'my-2 form-control'}
+                                       maxLength={250} autoResize
+                                       onChange={(Event) => {
+                                           setDescription(Event.target.value)
+                                       }}
+                        />
+                        <small id="description-help" className="text-start block">Enter job's description (max 250
+                            char).</small>
+                    </div>
+                    <div className="field">
+                        <label htmlFor="image" className="text-start block">Image url</label>
+                        <InputText type={'text'} id={'image'} className={'my-2 form-control'}
                                    onChange={(Event) => {
                                        setImage(Event.target.value)
                                    }}
                         />
-                                    <label htmlFor="image">Job image</label>
-                        </span>
-                    <button onClick={AddJop} className={'btn btn-primary'} type="submit">Add jop</button>
-
+                        <small id="username1-help" className="text-start block">Enter job's image (url format).</small>
+                    </div>
+                    <div className="field">
+                        <label htmlFor="tags" className="text-start block">tags</label>
+                        <MultiSelect id={"tags"} value={selectedTags} options={tags} className={'form-control'}
+                                     onChange={(Event) => setSelectedTags(Event.value)} optionLabel="name"
+                                     placeholder="Select tags" display="chip"/>
+                        <small id="username1-help" className="text-start block">Enter job's image (url format).</small>
+                    </div>
+                    <Button onClick={AddJop} className={'my-4 p-button p-button-rounded align-self-start'}
+                            type="submit">Add job</Button>
                 </form>
             </div>
         )
